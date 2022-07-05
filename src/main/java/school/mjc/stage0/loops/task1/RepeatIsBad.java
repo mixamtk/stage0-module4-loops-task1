760980359ba9648ee2de85347b115a5f5c9715d3
mixamtk <70889784+mixamtk@users.noreply.github.com>
2022-07-05 12:01:56 +0300

    Update RepeatIsBad.java

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -1,7 +1,11 @@
+
 package school.mjc.stage0.loops.task1;
+import java.util.stream.Stream;
+import java.util.function.Supplier;
 
 public class RepeatIsBad {
     public void repeatIsBad(){
-
+        Supplier<String> supplierStr = () ->  "writing the same code doesn't have much impact, and it's also time consuming";
+        Stream.generate( supplierStr).limit(20).forEach(System.out::println);
     }
 }
